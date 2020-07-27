fb722b9429bcb70f7738b07a44b39b1d299d8e94
gllm <special.geeee@gmail.com>
2020-07-28 01:45:22 +0200

    added Body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div className="body">
+                <p>BODY</p>
+            </div>
+        )
+    }
+}
+
+export default Body;
