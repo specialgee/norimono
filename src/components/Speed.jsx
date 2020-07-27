10f18abf0fda48a207b842bdda66680fdc9d97ea
gllm <special.geeee@gmail.com>
2020-07-28 01:45:07 +0200

    added Speed component

---
+++ src/components/Speed.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Speed extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div className="speed">
+                <p>SPEED</p>
+            </div>
+        )
+    }
+}
+
+export default Speed;
