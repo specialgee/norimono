b2a2e69c6220c8e97b5fc09405abc9027a454bf2
gllm <special.geeee@gmail.com>
2020-07-28 01:45:14 +0200

    added Settings component

---
+++ src/components/Settings.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div className="settings">
+                <p>SETTINGS</p>
+            </div>
+        )
+    }
+}
+
+export default Settings;
