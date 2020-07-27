656f1ecd1d9c8f69c889323c5d00cb7aa405bf10
gllm <special.geeee@gmail.com>
2020-07-28 01:44:52 +0200

    added Direction component

---
+++ src/components/Direction.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Direction extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div className="direction">
+                <p>DIRECTION</p>
+            </div>
+        )
+    }
+}
+
+export default Direction;
