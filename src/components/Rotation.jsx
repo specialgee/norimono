7b46d19f83147a43daf1b6bb1437674cac61bf4c
gllm <special.geeee@gmail.com>
2020-07-28 01:44:58 +0200

    added Rotation component

---
+++ src/components/Rotation.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Rotation extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div className="rotation">
+                <p>ROTATION</p>
+            </div>
+        )
+    }
+}
+
+export default Rotation;
