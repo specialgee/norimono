ec251f86a27b598b1a95f1d1fd39196c63544cfb
gllm <special.geeee@gmail.com>
2020-07-30 01:14:27 +0200

    updated Speed component

--- src/components/Speed.jsx
+++ src/components/Speed.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {Container, Row} from 'react-bootstrap'
+import speedArrow from '../assets/img/speed/speed-arrow.png'
+import audioSlow from '../assets/audio/speed/ゆっくり.wav'
+import audioFast from '../assets/audio/speed/早く.wav'
 
 class Speed extends Component {
     constructor(props) {
@@ -7,6 +11,8 @@ class Speed extends Component {
         this.state = {
 
         }
+
+        this.onButtonClick = this.onButtonClick.bind(this);
     }
 
     componentDidMount() {
@@ -17,10 +23,32 @@ class Speed extends Component {
 
     }
 
+    onButtonClick(e) {
+        e.target.children[0].play();
+    }
+
     render() {
         return (
             <div className="speed">
-                <p>SPEED</p>
+                <Container>
+                    <Row>
+                        <button className="speed-slow" onClick={this.onButtonClick}>
+                            ゆっくり
+                            <audio className="button-audio">
+                                <source src={audioSlow} type="audio/wav" />
+                                Your browser does not support the audio element.
+                            </audio>
+                        </button>
+                        <img className="speed-arrow" src={speedArrow} alt="speed arrow" />
+                        <button className="speed-fast" onClick={this.onButtonClick}>
+                            はやく
+                            <audio className="button-audio">
+                                <source src={audioFast} type="audio/wav" />
+                                Your browser does not support the audio element.
+                            </audio>
+                        </button>
+                    </Row>
+                </Container>
             </div>
         )
     }
