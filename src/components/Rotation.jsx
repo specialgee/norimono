493b165c842c223bad3f4a5846786e1e1a20ce9a
gllm <special.geeee@gmail.com>
2020-07-29 11:32:15 +0200

    updated Rotation component

--- src/components/Rotation.jsx
+++ src/components/Rotation.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {Container, Row} from 'react-bootstrap'
+import rotate30 from '../assets/audio/rotation/30°.wav'
+import rotate45 from '../assets/audio/rotation/45°.wav'
 
 class Rotation extends Component {
     constructor(props) {
@@ -7,6 +10,8 @@ class Rotation extends Component {
         this.state = {
 
         }
+
+        this.onButtonClick = this.onButtonClick.bind(this);
     }
 
     componentDidMount() {
@@ -17,10 +22,31 @@ class Rotation extends Component {
 
     }
 
+    onButtonClick(e) {
+        e.target.children[0].play();
+    }
+
     render() {
         return (
             <div className="rotation">
-                <p>ROTATION</p>
+                <Container >
+                    <Row>
+                        <button className="rotation-30" onClick={this.onButtonClick}>
+                            30°
+                            <audio className="button-audio">
+                                <source src={rotate30} type="audio/wav" />
+                                Your browser does not support the audio element.
+                            </audio>
+                        </button>
+                        <button className="rotation-45" onClick={this.onButtonClick}>
+                            45°
+                            <audio className="button-audio">
+                                <source src={rotate45} type="audio/wav" />
+                                Your browser does not support the audio element.
+                            </audio>
+                        </button>
+                    </Row>
+                </Container>
             </div>
         )
     }
